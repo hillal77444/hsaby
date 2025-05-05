da14581c35f891c1a897c1825d921a549d66d245
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 01:55:15 +0300

    Update TransactionViewModel.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/transactions/TransactionViewModel.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/transactions/TransactionViewModel.java
@@ -1,7 +1,11 @@
 package com.hillal.hhhhhhh.ui.transactions;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+import com.hillal.hhhhhhh.data.model.Transaction;
 import com.hillal.hhhhhhh.data.repository.AccountRepository;
+
+import java.util.List;
 
 public class TransactionViewModel extends ViewModel {
     private final AccountRepository accountRepository;
@@ -10,5 +14,23 @@ public class TransactionViewModel extends ViewModel {
         this.accountRepository = accountRepository;
     }
 
-    // Add transaction methods here
+    public LiveData<List<Transaction>> getTransactionsForAccount(int accountId) {
+        return accountRepository.getTransactionsForAccount(accountId);
+    }
+
+    public LiveData<Double> getAccountBalance(int accountId) {
+        return accountRepository.getAccountBalance(accountId);
+    }
+
+    public void insertTransaction(Transaction transaction) {
+        accountRepository.insertTransaction(transaction);
+    }
+
+    public void updateTransaction(Transaction transaction) {
+        accountRepository.updateTransaction(transaction);
+    }
+
+    public void deleteTransaction(Transaction transaction) {
+        accountRepository.deleteTransaction(transaction);
+    }
 } 
