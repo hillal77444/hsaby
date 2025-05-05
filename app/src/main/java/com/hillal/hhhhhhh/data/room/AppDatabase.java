aee5d14044f80266d9f8590270b0997e537f91d7
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 01:34:32 +0300

    Create AppDatabase.java

---
+++ app/src/main/java/com/hillal/hhhhhhh/data/room/AppDatabase.java
@@ -0,0 +1,49 @@
+package com.hillal.hhhhhhh.data.room;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.hillal.hhhhhhh.data.model.Account;
+
+@Database(entities = {Account.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    private static final String TAG = "AppDatabase";
+    private static final String DATABASE_NAME = "accounting_db";
+    private static volatile AppDatabase instance;
+
+    public abstract AccountDao accountDao();
+
+    public static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase.class,
+                    DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+            Log.d(TAG, "Database instance created");
+        }
+        return instance;
+    }
+
+    private static final RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            Log.d(TAG, "Database created");
+        }
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            Log.d(TAG, "Database opened");
+        }
+    };
+} 
