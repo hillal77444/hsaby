6a24057f9934e3f4a56cf1788a16b5e7a9a230a6
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 02:27:00 +0300

    Update TransactionDao.java

--- app/src/main/java/com/hillal/hhhhhhh/data/room/TransactionDao.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/room/TransactionDao.java
@@ -13,10 +13,10 @@ import java.util.List;
 @Dao
 public interface TransactionDao {
     @Query("SELECT * FROM transactions WHERE accountId = :accountId ORDER BY date DESC")
-    LiveData<List<Transaction>> getTransactionsForAccount(int accountId);
+    LiveData<List<Transaction>> getTransactionsForAccount(long accountId);
 
     @Query("SELECT COALESCE(SUM(CASE WHEN isDebit = 1 THEN amount ELSE -amount END), 0) FROM transactions WHERE accountId = :accountId")
-    LiveData<Double> getAccountBalance(int accountId);
+    LiveData<Double> getAccountBalance(long accountId);
 
     @Insert
     void insertTransaction(Transaction transaction);
