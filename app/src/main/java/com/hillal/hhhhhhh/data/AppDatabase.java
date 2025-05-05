29622fa58496b925311c8b3763377b87703b1c2a
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 00:18:33 +0300

    Update AppDatabase.java

--- app/src/main/java/com/hillal/hhhhhhh/data/AppDatabase.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/AppDatabase.java
@@ -3,6 +3,7 @@ package com.hillal.hhhhhhh.data;
 import android.content.Context;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
