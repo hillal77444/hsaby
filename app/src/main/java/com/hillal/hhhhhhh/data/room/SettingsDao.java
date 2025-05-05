a9c2c59142e30b6d545cf5b6357f63b352b37936
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 02:33:42 +0300

    Create SettingsDao.java

---
+++ app/src/main/java/com/hillal/hhhhhhh/data/room/SettingsDao.java
@@ -0,0 +1,28 @@
+package com.hillal.hhhhhhh.data.room;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.hillal.hhhhhhh.data.model.Settings;
+import java.util.List;
+
+@Dao
+public interface SettingsDao {
+    @Insert
+    void insert(Settings settings);
+
+    @Update
+    void update(Settings settings);
+
+    @Delete
+    void delete(Settings settings);
+
+    @Query("SELECT * FROM settings ORDER BY key ASC")
+    LiveData<List<Settings>> getAllSettings();
+
+    @Query("SELECT * FROM settings WHERE key = :key")
+    LiveData<Settings> getSettingByKey(String key);
+} 
