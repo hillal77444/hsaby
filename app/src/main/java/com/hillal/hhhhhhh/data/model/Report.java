eaa1473c945c431e8775650b55be53fea0a7f506
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-07 01:49:01 +0300

    Create Report.java

---
+++ app/src/main/java/com/hillal/hhhhhhh/data/model/Report.java
@@ -0,0 +1,47 @@
+package com.hillal.hhhhhhh.data.model;
+
+public class Report {
+    private String title;
+    private String description;
+    private double amount;
+    private String date;
+
+    public Report(String title, String description, double amount, String date) {
+        this.title = title;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+} 
