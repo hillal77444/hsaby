0b0de8d5ae5d6c3f8a7a9a8a72aee4666bf13fcd
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 20:54:04 +0300

    Update SettingsFragment.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/settings/SettingsFragment.java
@@ -31,7 +31,8 @@ public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
