64ac8d6de57a369cecdb85c145a835865220628d
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-07 02:19:32 +0300

    Update ReportAdapter.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/reports/ReportAdapter.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/reports/ReportAdapter.java
@@ -52,10 +52,10 @@ public class ReportAdapter extends RecyclerView.Adapter<ReportAdapter.ViewHolder
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
-            dateTextView = itemView.findViewById(R.id.transaction_date);
-            typeTextView = itemView.findViewById(R.id.transaction_type);
-            amountTextView = itemView.findViewById(R.id.transaction_amount);
-            descriptionTextView = itemView.findViewById(R.id.transaction_description);
+            dateTextView = itemView.findViewById(R.id.transactionDate);
+            typeTextView = itemView.findViewById(R.id.transactionType);
+            amountTextView = itemView.findViewById(R.id.transactionAmount);
+            descriptionTextView = itemView.findViewById(R.id.transactionDescription);
         }
 
         void bind(Transaction transaction) {
