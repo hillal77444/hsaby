d6766d9fd53776d443aeaa16a8bfb2e10bd4d00b
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-06 16:56:13 +0300

    Update SettingsRepository.java

--- app/src/main/java/com/hillal/hhhhhhh/data/repository/SettingsRepository.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/repository/SettingsRepository.java
@@ -2,36 +2,37 @@ package com.hillal.hhhhhhh.data.repository;
 
 import android.app.Application;
 import androidx.lifecycle.LiveData;
+import com.hillal.hhhhhhh.App;
 import com.hillal.hhhhhhh.data.model.Settings;
-import com.hillal.hhhhhhh.data.room.AppDatabase;
 import com.hillal.hhhhhhh.data.room.SettingsDao;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SettingsRepository {
     private final SettingsDao settingsDao;
+    private final LiveData<Settings> settings;
     private final ExecutorService executorService;
 
     public SettingsRepository(Application application) {
-        AppDatabase database = AppDatabase.getInstance(application);
-        settingsDao = database.settingsDao();
+        App app = (App) application;
+        settingsDao = app.getDatabase().settingsDao();
+        settings = settingsDao.getSettings();
         executorService = Executors.newSingleThreadExecutor();
     }
 
-    public LiveData<List<Settings>> getAllSettings() {
-        return settingsDao.getAllSettings();
+    public LiveData<Settings> getSettings() {
+        return settings;
     }
 
-    public void insertSettings(Settings settings) {
+    public void insert(Settings settings) {
         executorService.execute(() -> settingsDao.insert(settings));
     }
 
-    public void updateSettings(Settings settings) {
+    public void update(Settings settings) {
         executorService.execute(() -> settingsDao.update(settings));
     }
 
-    public void deleteSettings(Settings settings) {
+    public void delete(Settings settings) {
         executorService.execute(() -> settingsDao.delete(settings));
     }
 } 
