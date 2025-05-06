889d48cf96779038a7d29b3150633451958f2894
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-07 00:11:43 +0300

    Update AccountStatementFragment.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/reports/AccountStatementFragment.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/reports/AccountStatementFragment.java
@@ -172,25 +172,67 @@ public class AccountStatementFragment extends Fragment {
             Font titleFont = new Font(Font.FontFamily.HELVETICA, 18, Font.BOLD);
             Paragraph title = new Paragraph(accountNameText.getText().toString(), titleFont);
             title.setAlignment(Element.ALIGN_CENTER);
+            title.setSpacingAfter(20);
             document.add(title);
 
             // Add date range
             Paragraph dateRange = new Paragraph(getString(R.string.period, 
                 DATE_FORMAT.format(fromDate), DATE_FORMAT.format(toDate)));
             dateRange.setAlignment(Element.ALIGN_CENTER);
+            dateRange.setSpacingAfter(20);
             document.add(dateRange);
 
-            // Add balance
-            Paragraph balance = new Paragraph(accountBalanceText.getText().toString());
-            balance.setAlignment(Element.ALIGN_RIGHT);
-            document.add(balance);
+            // Calculate totals
+            double totalDebit = 0;
+            double totalCredit = 0;
+            List<Transaction> transactions = transactionsAdapter.getTransactions();
+            for (Transaction transaction : transactions) {
+                if ("مدين".equals(transaction.getType())) {
+                    totalDebit += transaction.getAmount();
+                } else {
+                    totalCredit += transaction.getAmount();
+                }
+            }
+
+            // Add summary
+            Font summaryFont = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+            Paragraph summary = new Paragraph();
+            summary.setSpacingBefore(20);
+            summary.setSpacingAfter(20);
+            
+            // Add opening balance
+            Account account = accountViewModel.getAccountById(accountId).getValue();
+            if (account != null) {
+                summary.add(new Paragraph(getString(R.string.opening_balance, 
+                    String.format("%.2f", account.getOpeningBalance())), summaryFont));
+                summary.add(new Paragraph("\n"));
+            }
+
+            // Add totals
+            summary.add(new Paragraph(getString(R.string.total_debit, 
+                String.format("%.2f", totalDebit)), summaryFont));
+            summary.add(new Paragraph(getString(R.string.total_credit, 
+                String.format("%.2f", totalCredit)), summaryFont));
+            summary.add(new Paragraph(accountBalanceText.getText().toString(), summaryFont));
+            document.add(summary);
+
+            // Add transactions table header
+            Paragraph tableHeader = new Paragraph("التاريخ | النوع | المبلغ | البيان");
+            tableHeader.setAlignment(Element.ALIGN_RIGHT);
+            tableHeader.setSpacingBefore(20);
+            tableHeader.setSpacingAfter(10);
+            document.add(tableHeader);
 
             // Add transactions
-            for (Transaction transaction : transactionsAdapter.getTransactions()) {
-                Paragraph transactionText = new Paragraph(String.format("%s - %s - %s",
+            Font transactionFont = new Font(Font.FontFamily.HELVETICA, 10);
+            for (Transaction transaction : transactions) {
+                Paragraph transactionText = new Paragraph(String.format("%s | %s | %s | %s",
                     DATE_FORMAT.format(transaction.getDate()),
                     transaction.getType(),
-                    String.format("%.2f", transaction.getAmount())));
+                    String.format("%.2f", transaction.getAmount()),
+                    transaction.getDescription()), transactionFont);
+                transactionText.setAlignment(Element.ALIGN_RIGHT);
+                transactionText.setSpacingAfter(5);
                 document.add(transactionText);
             }
 
