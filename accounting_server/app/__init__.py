94a4a1543642679a1e0dfe67867d27973154ecb0
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-08 00:30:16 +0300

    Update __init__.py

--- accounting_server/app/__init__.py
+++ accounting_server/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
 jwt = JWTManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -14,6 +16,7 @@ def create_app():
     # تهيئة الإضافات
     db.init_app(app)
     jwt.init_app(app)
+    migrate.init_app(app, db)
     CORS(app)
     
     # تسجيل المسارات
