cdbbb2f096cd72c9cf0a8e2a0ac0dd5535b2a7e6
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-07 16:14:53 +0300

    Update TransactionDao.java

--- app/src/main/java/com/hillal/hhhhhhh/data/dao/TransactionDao.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/dao/TransactionDao.java
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Transaction;
 
 import com.hillal.hhhhhhh.data.model.Transaction;
 
