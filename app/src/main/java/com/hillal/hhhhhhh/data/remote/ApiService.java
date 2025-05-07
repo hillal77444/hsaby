781c38b44b8f2d46a7ebefed3a81f93a0a2d9e13
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-08 01:16:36 +0300

    Create ApiService.java

---
+++ app/src/main/java/com/hillal/hhhhhhh/data/remote/ApiService.java
@@ -0,0 +1,38 @@
+package com.hillal.hhhhhhh.data.remote;
+
+import com.hillal.hhhhhhh.data.model.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiService {
+    @POST("auth/login")
+    Call<User> login(@Body LoginRequest request);
+
+    @POST("auth/register")
+    Call<User> register(@Body RegisterRequest request);
+
+    @POST("sync/data")
+    Call<Void> syncData();
+
+    class LoginRequest {
+        private String phone;
+        private String password;
+
+        public LoginRequest(String phone, String password) {
+            this.phone = phone;
+            this.password = password;
+        }
+    }
+
+    class RegisterRequest {
+        private String phone;
+        private String password;
+
+        public RegisterRequest(String phone, String password) {
+            this.phone = phone;
+            this.password = password;
+        }
+    }
+} 
