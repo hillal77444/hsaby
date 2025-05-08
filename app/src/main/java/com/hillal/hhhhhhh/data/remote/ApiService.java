1635aa9b57dcfdbfd7b381cf396921521ca84ffc
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-08 17:30:33 +0300

    Update ApiService.java

--- app/src/main/java/com/hillal/hhhhhhh/data/remote/ApiService.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/remote/ApiService.java
@@ -1,10 +1,15 @@
 package com.hillal.hhhhhhh.data.remote;
 
 import com.hillal.hhhhhhh.data.model.User;
+import com.hillal.hhhhhhh.data.model.Account;
+import com.hillal.hhhhhhh.data.model.Transaction;
 
+import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 
 public interface ApiService {
     @POST("api/login")
@@ -13,8 +18,14 @@ public interface ApiService {
     @POST("api/register")
     Call<User> register(@Body RegisterRequest request);
 
-    @POST("api/sync/data")
-    Call<Void> syncData();
+    @POST("api/sync")
+    Call<Void> syncData(@Body SyncRequest request);
+
+    @GET("api/accounts")
+    Call<List<Account>> getAccounts(@Header("Authorization") String token);
+
+    @GET("api/transactions")
+    Call<List<Transaction>> getTransactions(@Header("Authorization") String token);
 
     class LoginRequest {
         private String phone;
@@ -35,6 +46,16 @@ public interface ApiService {
             this.username = username;
             this.phone = phone;
             this.password = password;
+        }
+    }
+
+    class SyncRequest {
+        private List<Account> accounts;
+        private List<Transaction> transactions;
+
+        public SyncRequest(List<Account> accounts, List<Transaction> transactions) {
+            this.accounts = accounts;
+            this.transactions = transactions;
         }
     }
 } 
