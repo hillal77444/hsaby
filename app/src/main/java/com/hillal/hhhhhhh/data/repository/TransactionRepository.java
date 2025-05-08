685060b3692263060af0714852c1cc4028764c80
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-08 18:12:47 +0300

    Update TransactionRepository.java

--- app/src/main/java/com/hillal/hhhhhhh/data/repository/TransactionRepository.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/repository/TransactionRepository.java
@@ -2,9 +2,10 @@ package com.hillal.hhhhhhh.data.repository;
 
 import android.app.Application;
 import androidx.lifecycle.LiveData;
-import com.hillal.hhhhhhh.data.model.Transaction;
-import com.hillal.hhhhhhh.data.room.AppDatabase;
+import com.hillal.hhhhhhh.data.AppDatabase;
 import com.hillal.hhhhhhh.data.room.TransactionDao;
+import com.hillal.hhhhhhh.data.model.Transaction;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,44 +20,56 @@ public class TransactionRepository {
         executorService = Executors.newSingleThreadExecutor();
     }
 
+    public LiveData<List<Transaction>> getTransactionsForAccount(long accountId) {
+        return transactionDao.getTransactionsForAccount(accountId);
+    }
+
+    public LiveData<List<Transaction>> getTransactionsForAccountInDateRange(long accountId, Date startDate, Date endDate) {
+        return transactionDao.getTransactionsForAccountInDateRange(accountId, startDate, endDate);
+    }
+
+    public LiveData<Double> getTotalCredits(long accountId) {
+        return transactionDao.getTotalCredits(accountId);
+    }
+
+    public LiveData<Double> getTotalDebits(long accountId) {
+        return transactionDao.getTotalDebits(accountId);
+    }
+
+    public LiveData<Transaction> getLastTransaction(long accountId) {
+        return transactionDao.getLastTransaction(accountId);
+    }
+
+    public LiveData<Double> getTotalCreditsInDateRange(long accountId, Date startDate, Date endDate) {
+        return transactionDao.getTotalCreditsInDateRange(accountId, startDate, endDate);
+    }
+
+    public LiveData<Double> getTotalDebitsInDateRange(long accountId, Date startDate, Date endDate) {
+        return transactionDao.getTotalDebitsInDateRange(accountId, startDate, endDate);
+    }
+
     public LiveData<List<Transaction>> getAllTransactions() {
         return transactionDao.getAllTransactions();
-    }
-
-    public LiveData<List<Transaction>> getTransactionsByAccount(long accountId) {
-        return transactionDao.getTransactionsByAccount(accountId);
-    }
-
-    public LiveData<List<Transaction>> getTransactionsByType(String type) {
-        return transactionDao.getTransactionsByType(type);
-    }
-
-    public LiveData<List<Transaction>> getTransactionsByCurrency(String currency) {
-        return transactionDao.getTransactionsByCurrency(currency);
     }
 
     public LiveData<Double> getAccountBalance(long accountId) {
         return transactionDao.getAccountBalance(accountId);
     }
 
-    public void insert(Transaction transaction) {
-        executorService.execute(() -> transactionDao.insert(transaction));
+    public LiveData<Transaction> getTransactionById(long transactionId) {
+        return transactionDao.getTransactionById(transactionId);
     }
 
-    public void update(Transaction transaction) {
-        executorService.execute(() -> transactionDao.update(transaction));
-    }
-
-    public void delete(Transaction transaction) {
-        executorService.execute(() -> transactionDao.delete(transaction));
-    }
-
-    public LiveData<Transaction> getById(long id) {
-        return transactionDao.getTransactionById(id);
+    public LiveData<List<Transaction>> getTransactionsByType(String type) {
+        return transactionDao.getTransactionsByType(type);
     }
 
     public LiveData<List<Transaction>> getTransactionsByDateRange(long fromDate, long toDate) {
         return transactionDao.getTransactionsByDateRange(fromDate, toDate);
+    }
+
+    public LiveData<List<Transaction>> getTransactionsByAccount(long accountId) {
+        return transactionDao.getTransactionsByAccount(accountId);
     }
 
     public LiveData<List<Transaction>> getTransactionsByAccountAndDateRange(long accountId, long fromDate, long toDate) {
@@ -69,5 +82,21 @@ public class TransactionRepository {
 
     public LiveData<Double> getTotalCredit(long accountId) {
         return transactionDao.getTotalCredit(accountId);
+    }
+
+    public List<Transaction> getTransactionsModifiedAfter(long timestamp) {
+        return transactionDao.getTransactionsModifiedAfter(timestamp);
+    }
+
+    public void insert(Transaction transaction) {
+        executorService.execute(() -> transactionDao.insert(transaction));
+    }
+
+    public void update(Transaction transaction) {
+        executorService.execute(() -> transactionDao.update(transaction));
+    }
+
+    public void delete(Transaction transaction) {
+        executorService.execute(() -> transactionDao.delete(transaction));
     }
 }
