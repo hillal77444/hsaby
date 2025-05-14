67ed8f9ad94ccbd31af98e7d527065ffbbe6f171
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-14 21:19:41 +0300

    Create Migration_2.java

---
+++ app/src/main/java/com/hillal/hhhhhhh/data/room/migrations/Migration_2.java
@@ -0,0 +1,16 @@
+package com.hillal.hhhhhhh.data.room.migrations;
+
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+public class Migration_2 extends Migration {
+    public Migration_2() {
+        super(1, 2);
+    }
+
+    @Override
+    public void migrate(SupportSQLiteDatabase database) {
+        // إضافة عمود whatsapp_enabled إلى جدول transactions
+        database.execSQL("ALTER TABLE transactions ADD COLUMN whatsappEnabled INTEGER NOT NULL DEFAULT 1");
+    }
+} 
