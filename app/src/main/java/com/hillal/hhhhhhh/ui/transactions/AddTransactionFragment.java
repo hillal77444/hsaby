d16232c524e64c4a6daf94fce8f662c9163c14a5
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-16 22:48:07 +0300

    Update AddTransactionFragment.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/transactions/AddTransactionFragment.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/transactions/AddTransactionFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
