7d3a1301cb65e94ff959b70e746cda05b55e91a4
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-18 01:29:24 +0300

    Update TransactionDao.java

--- app/src/main/java/com/hillal/hhhhhhh/data/room/TransactionDao.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/room/TransactionDao.java
@@ -49,6 +49,9 @@ public interface TransactionDao {
     @Query("SELECT * FROM transactions ORDER BY date DESC")
     LiveData<List<Transaction>> getAllTransactions();
 
+    @Query("SELECT * FROM transactions")
+    List<Transaction> getAllTransactionsSync();
+
     @Query("SELECT SUM(CASE WHEN type = 'debit' THEN amount ELSE -amount END) FROM transactions WHERE accountId = :accountId")
     LiveData<Double> getAccountBalance(long accountId);
 
