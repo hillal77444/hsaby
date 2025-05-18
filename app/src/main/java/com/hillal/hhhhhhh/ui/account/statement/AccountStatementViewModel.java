031e69aec24c9af14f2c19dd218dd2391f6a11ce
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-18 16:51:46 +0300

    Update AccountStatementViewModel.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/account/statement/AccountStatementViewModel.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/account/statement/AccountStatementViewModel.java
@@ -1,3 +1,16 @@
-public double getAccountBalance(long accountId) {
-    return repository.getAccountBalance(accountId);
+package com.hillal.hhhhhhh.ui.account.statement;
+
+import androidx.lifecycle.ViewModel;
+import com.hillal.hhhhhhh.data.repository.AccountStatementRepository;
+
+public class AccountStatementViewModel extends ViewModel {
+    private final AccountStatementRepository repository;
+
+    public AccountStatementViewModel(AccountStatementRepository repository) {
+        this.repository = repository;
+    }
+
+    public double getAccountBalance(long accountId) {
+        return repository.getAccountBalance(accountId);
+    }
 } 
