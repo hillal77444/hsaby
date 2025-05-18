8ca4fabc33215f4a4e24195a65825d29ab602336
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-19 02:20:55 +0300

    Update MainActivity.java

--- app/src/main/java/com/hillal/hhhhhhh/MainActivity.java
+++ app/src/main/java/com/hillal/hhhhhhh/MainActivity.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
