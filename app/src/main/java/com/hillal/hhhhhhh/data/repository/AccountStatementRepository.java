c5762648485a60892a9bae21be085f934bd23c6d
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-18 16:51:43 +0300

    Update AccountStatementRepository.java

--- app/src/main/java/com/hillal/hhhhhhh/data/repository/AccountStatementRepository.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/repository/AccountStatementRepository.java
@@ -1,3 +1,22 @@
-public double getAccountBalance(long accountId) {
-    return transactionDao.getAccountBalanceSync(accountId);
+package com.hillal.hhhhhhh.data.repository;
+
+import com.hillal.hhhhhhh.data.dao.AccountDao;
+import com.hillal.hhhhhhh.data.dao.TransactionDao;
+import com.hillal.hhhhhhh.data.model.Account;
+import com.hillal.hhhhhhh.data.model.Transaction;
+
+import java.util.List;
+
+public class AccountStatementRepository {
+    private final TransactionDao transactionDao;
+    private final AccountDao accountDao;
+
+    public AccountStatementRepository(TransactionDao transactionDao, AccountDao accountDao) {
+        this.transactionDao = transactionDao;
+        this.accountDao = accountDao;
+    }
+
+    public double getAccountBalance(long accountId) {
+        return transactionDao.getAccountBalanceSync(accountId);
+    }
 } 
