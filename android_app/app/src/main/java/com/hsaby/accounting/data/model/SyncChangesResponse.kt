51c258860e3531557ba20fa34d1428a3493a27a6
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-20 23:00:48 +0300

    Delete SyncChangesResponse.kt

--- android_app/app/src/main/java/com/hsaby/accounting/data/model/SyncChangesResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.hsaby.accounting.data.model
-
-data class SyncChangesResponse(
-    val success: Boolean,
-    val message: String,
-    val syncedAccounts: List<Account>,
-    val syncedTransactions: List<Transaction>
-) 
