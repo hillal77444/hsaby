71d088d3c4f1e82bd714bc3c5b4547e9cabdfac0
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-21 19:42:26 +0300

    Update Models.kt

--- android_app/app/src/main/java/com/hsaby/accounting/data/model/Models.kt
+++ android_app/app/src/main/java/com/hsaby/accounting/data/model/Models.kt
@@ -4,14 +4,14 @@ import com.google.gson.annotations.SerializedName
 
 // Request Models
 data class LoginRequest(
-    @SerializedName("phone") val phone: String,
-    @SerializedName("password") val password: String
+    val phone: String,
+    val password: String
 )
 
 data class RegisterRequest(
-    @SerializedName("phone") val phone: String,
-    @SerializedName("password") val password: String,
-    @SerializedName("name") val name: String
+    val name: String,
+    val phone: String,
+    val password: String
 )
 
 data class SyncRequest(
@@ -25,15 +25,15 @@ data class SyncChangesRequest(
 
 // Response Models
 data class LoginResponse(
-    @SerializedName("token") val token: String,
-    @SerializedName("refresh_token") val refreshToken: String,
-    @SerializedName("user") val user: User
+    val token: String,
+    val refreshToken: String,
+    val user: User
 )
 
 data class RegisterResponse(
-    @SerializedName("token") val token: String,
-    @SerializedName("refresh_token") val refreshToken: String,
-    @SerializedName("user") val user: User
+    val token: String,
+    val refreshToken: String,
+    val user: User
 )
 
 data class SyncResponse(
@@ -90,7 +90,7 @@ data class Transaction(
 )
 
 data class User(
-    @SerializedName("id") val id: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("phone") val phone: String
+    val id: String,
+    val name: String,
+    val phone: String
 ) 
