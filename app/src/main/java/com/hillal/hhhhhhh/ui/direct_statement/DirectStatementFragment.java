496e4e07245bbfbb2e01fda8597d08b7dcdd5ecf
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-27 00:03:20 +0300

    Update DirectStatementFragment.java

--- app/src/main/java/com/hillal/hhhhhhh/ui/direct_statement/DirectStatementFragment.java
+++ app/src/main/java/com/hillal/hhhhhhh/ui/direct_statement/DirectStatementFragment.java
@@ -50,7 +50,7 @@ public class DirectStatementFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.direct.fragment_direct_statement, container, false);
+        View view = inflater.inflate(R.layout.fragment_direct_statement, container, false);
         
         // تهيئة عناصر واجهة المستخدم
         initializeViews(view);
