2e660a6a2bb12808a2249c9e1eff7c5f4fb521c4
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-27 01:33:07 +0300

    Update AccountSummaryResponse.java

--- app/src/main/java/com/hillal/hhhhhhh/data/model/AccountSummaryResponse.java
+++ app/src/main/java/com/hillal/hhhhhhh/data/model/AccountSummaryResponse.java
@@ -1,71 +1,90 @@
 package com.hillal.hhhhhhh.data.model;
 
-import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class AccountSummaryResponse {
-    @SerializedName("accounts")
     private List<AccountSummary> accounts;
-
-    @SerializedName("totalBalance")
     private double totalBalance;
-
-    @SerializedName("totalDebits")
     private double totalDebits;
-
-    @SerializedName("totalCredits")
     private double totalCredits;
 
     public List<AccountSummary> getAccounts() {
         return accounts;
     }
 
+    public void setAccounts(List<AccountSummary> accounts) {
+        this.accounts = accounts;
+    }
+
     public double getTotalBalance() {
         return totalBalance;
+    }
+
+    public void setTotalBalance(double totalBalance) {
+        this.totalBalance = totalBalance;
     }
 
     public double getTotalDebits() {
         return totalDebits;
     }
 
+    public void setTotalDebits(double totalDebits) {
+        this.totalDebits = totalDebits;
+    }
+
     public double getTotalCredits() {
         return totalCredits;
     }
 
+    public void setTotalCredits(double totalCredits) {
+        this.totalCredits = totalCredits;
+    }
+
     public static class AccountSummary {
-        @SerializedName("userId")
         private long userId;
-
-        @SerializedName("userName")
         private String userName;
-
-        @SerializedName("balance")
         private double balance;
-
-        @SerializedName("totalDebits")
         private double totalDebits;
-
-        @SerializedName("totalCredits")
         private double totalCredits;
 
         public long getUserId() {
             return userId;
         }
 
+        public void setUserId(long userId) {
+            this.userId = userId;
+        }
+
         public String getUserName() {
             return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
         }
 
         public double getBalance() {
             return balance;
         }
 
+        public void setBalance(double balance) {
+            this.balance = balance;
+        }
+
         public double getTotalDebits() {
             return totalDebits;
         }
 
+        public void setTotalDebits(double totalDebits) {
+            this.totalDebits = totalDebits;
+        }
+
         public double getTotalCredits() {
             return totalCredits;
+        }
+
+        public void setTotalCredits(double totalCredits) {
+            this.totalCredits = totalCredits;
         }
     }
 } 
