1cd545ef924b76b6c2289bf938347341c191260b
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-05-27 03:07:47 +0300

    Update TransactionViewModel.java

--- app/src/main/java/com/hillal/hhhhhhh/viewmodel/TransactionViewModel.java
+++ app/src/main/java/com/hillal/hhhhhhh/viewmodel/TransactionViewModel.java
@@ -5,7 +5,8 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import com.hillal.hhhhhhh.data.model.Transaction;
 import com.hillal.hhhhhhh.data.room.AppDatabase;
-import com.hillal.hhhhhhh.repository.TransactionRepository;
+import com.hillal.hhhhhhh.data.repository.TransactionRepository;
+import java.util.Date;
 import java.util.List;
 
 public class TransactionViewModel extends AndroidViewModel {
@@ -14,7 +15,7 @@ public class TransactionViewModel extends AndroidViewModel {
     public TransactionViewModel(Application application) {
         super(application);
         AppDatabase database = AppDatabase.getInstance(application);
-        repository = new TransactionRepository(database.transactionDao());
+        repository = new TransactionRepository(database);
     }
 
     public void insert(Transaction transaction) {
@@ -51,5 +52,17 @@ public class TransactionViewModel extends AndroidViewModel {
 
     public LiveData<Double> getTotalCredit(long accountId) {
         return repository.getTotalCredit(accountId);
+    }
+
+    public LiveData<Double> getTotalCreditForDateRange(long accountId, Date startDate, Date endDate) {
+        return repository.getTotalCreditsInDateRange(accountId, startDate.getTime(), endDate.getTime());
+    }
+
+    public LiveData<Double> getTotalDebitForDateRange(long accountId, Date startDate, Date endDate) {
+        return repository.getTotalDebitsInDateRange(accountId, startDate.getTime(), endDate.getTime());
+    }
+
+    public LiveData<Double> getAccountBalance(long accountId) {
+        return repository.getAccountBalance(accountId);
     }
 } 
