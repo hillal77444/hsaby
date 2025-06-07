7052c66fd5be8d60eaf3650cfebd5cb52b229e25
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-06-08 02:53:45 +0300

    Update MigrationManager.java

--- app/src/main/java/com/hillal/acc/data/sync/MigrationManager.java
+++ app/src/main/java/com/hillal/acc/data/sync/MigrationManager.java
@@ -114,7 +114,7 @@ public class MigrationManager {
                                             transaction.setSyncStatus(2);
                                             
                                             // حذف المعاملة القديمة وإدخال المعاملة الجديدة
-                                            Transaction oldTransaction = transactionDao.getTransactionByIdSync(oldId);
+                                            Transaction oldTransaction = transactionDao.getTransactionById(oldId);
                                             if (oldTransaction != null) {
                                                 transactionDao.delete(oldTransaction);
                                             }
