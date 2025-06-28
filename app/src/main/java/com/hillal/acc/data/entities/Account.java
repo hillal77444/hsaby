98afc9c40cc1a264eaa1912f2153b691e80cf475
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-06-28 20:23:52 +0300

    Revert "Delete Account.java"
    This reverts commit ef7b0a3acdb748d709a96b979408804b660d4249.

---
+++ app/src/main/java/com/hillal/acc/data/entities/Account.java
@@ -0,0 +1,51 @@
+package com.hillal.acc.data.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "accounts")
+public class Account {
+    @PrimaryKey(autoGenerate = true)
+    private long id;
+    private String name;
+    private String type; // "debtor" or "creditor"
+    private double balance;
+
+    public Account(String name, String type, double balance) {
+        this.name = name;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+} 
