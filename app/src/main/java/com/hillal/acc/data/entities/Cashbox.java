d72c80e26211a7e713db8e9766fb8bc07954387a
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-06-28 21:51:43 +0300

    Update Cashbox.java

--- app/src/main/java/com/hillal/acc/data/entities/Cashbox.java
+++ app/src/main/java/com/hillal/acc/data/entities/Cashbox.java
@@ -3,12 +3,16 @@ package com.hillal.acc.data.entities;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.annotation.NonNull;
+import com.google.gson.annotations.SerializedName;
 
 @Entity(tableName = "cashboxes")
 public class Cashbox {
     @PrimaryKey
     @NonNull
+    @SerializedName("id")
     public int id;
+    @SerializedName("name")
     public String name;
+    @SerializedName("created_at")
     public String createdAt;
 } 
