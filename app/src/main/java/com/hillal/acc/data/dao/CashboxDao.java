1052a489371aefe56cfdd7c0fc3720c7b2e3a6bd
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-06-28 18:23:55 +0300

    Create CashboxDao.java

---
+++ app/src/main/java/com/hillal/acc/data/dao/CashboxDao.java
@@ -0,0 +1,29 @@
+package com.hillal.acc.data.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import androidx.room.Delete;
+
+import com.hillal.acc.data.entities.Cashbox;
+
+import java.util.List;
+
+@Dao
+public interface CashboxDao {
+    @Query("SELECT * FROM Cashbox")
+    List<Cashbox> getAll();
+
+    @Insert
+    void insert(Cashbox cashbox);
+
+    @Update
+    void update(Cashbox cashbox);
+
+    @Delete
+    void delete(Cashbox cashbox);
+
+    @Query("DELETE FROM Cashbox")
+    void deleteAll();
+} 
