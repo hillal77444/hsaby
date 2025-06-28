6decf7f341c6b2918386f4898b561e29fbbef604
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-06-28 18:26:27 +0300

    Update ApiService.java

--- app/src/main/java/com/hillal/acc/network/ApiService.java
+++ app/src/main/java/com/hillal/acc/network/ApiService.java
@@ -2,6 +2,9 @@ package com.hillal.acc.network;
 
 import com.hillal.acc.models.AccountSummaryResponse;
 import com.hillal.acc.models.AccountReport;
+import com.hillal.acc.data.entities.Cashbox;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -17,4 +20,7 @@ public interface ApiService {
 
     @GET("api/accounts/{accountId}/report")
     Call<AccountReport> getAccountReport(@Path("accountId") int accountId, @Query("currency") String currency);
+
+    @GET("api/cashboxes")
+    Call<List<Cashbox>> getCashboxes();
 } 
