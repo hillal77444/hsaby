91c154dccff0b15e2aa45a35874a647d321675dd
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-07-02 00:36:22 +0300

    Update AccountSummaryFragment.java

--- app/src/main/java/com/hillal/acc/ui/summary/AccountSummaryFragment.java
+++ app/src/main/java/com/hillal/acc/ui/summary/AccountSummaryFragment.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.core.view.ViewCompat;
