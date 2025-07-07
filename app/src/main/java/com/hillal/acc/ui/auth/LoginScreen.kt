c8595d4912d0729279c5f93d38623fb82f9eb69c
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-07-07 20:35:19 +0300

    Update LoginScreen.kt

--- app/src/main/java/com/hillal/acc/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/hillal/acc/ui/auth/LoginScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.hillal.acc.R
