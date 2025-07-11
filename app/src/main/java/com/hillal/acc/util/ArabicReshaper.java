10d2fee5cd5e0a7b939acccd952bb7874fd0168d
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-07-11 16:10:10 +0300

    Update ArabicReshaper.java

--- app/src/main/java/com/hillal/acc/util/ArabicReshaper.java
+++ app/src/main/java/com/hillal/acc/util/ArabicReshaper.java
@@ -2,6 +2,6 @@ package com.hillal.acc.util;
 
 public class ArabicReshaper {
     public static String reshape(String input) {
-        return BetterArabicReshaper.reshapeArabic(input);
+        return BetterArabicReshaper.reshapeAndBidi(input);
     }
 } 
