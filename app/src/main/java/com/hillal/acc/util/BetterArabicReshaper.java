8c809cbb4781c9659cdc5e87f9acfc20a7c19192
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-07-11 18:02:28 +0300

    ى

--- app/src/main/java/com/hillal/acc/util/BetterArabicReshaper.java
+++ app/src/main/java/com/hillal/acc/util/BetterArabicReshaper.java
@@ -1,83 +1,65 @@
 package com.hillal.acc.util;
 
 import java.text.Bidi;
-import java.util.HashMap;
-import java.util.Map;
 
 public class BetterArabicReshaper {
-    // --- بداية كود Better-Arabic-Reshaper (Java port) ---
-    // المصدر: https://github.com/AhmedEssam/Better-Arabic-Reshaper
-    // تم التبسيط والدمج مع المشروع الحالي
+    // كود ربط الحروف العربية (مقتبس من Better-Arabic-Reshaper - نسخة مبسطة)
+    // يدعم معظم الحروف العربية الشائعة ويكفي للتقارير المالية
+    // ملاحظة: هذا الكود لا يدعم التشكيل أو كل الرموز النادرة، لكنه كافٍ للنصوص المالية
 
-    // Presentation Forms-B Unicode
-    private static final char[] ARABIC_GLYPHS = {
-        '\uFE80', '\uFE81', '\uFE82', '\uFE83', '\uFE84', '\uFE85', '\uFE86', '\uFE87', '\uFE88',
-        '\uFE89', '\uFE8A', '\uFE8B', '\uFE8C', '\uFE8D', '\uFE8E', '\uFE8F', '\uFE90', '\uFE91',
-        '\uFE92', '\uFE93', '\uFE94', '\uFE95', '\uFE96', '\uFE97', '\uFE98', '\uFE99', '\uFE9A',
-        '\uFE9B', '\uFE9C', '\uFE9D', '\uFE9E', '\uFE9F', '\uFEA0', '\uFEA1', '\uFEA2', '\uFEA3',
-        '\uFEA4', '\uFEA5', '\uFEA6', '\uFEA7', '\uFEA8', '\uFEA9', '\uFEAA', '\uFEAB', '\uFEAC',
-        '\uFEAD', '\uFEAE', '\uFEAF', '\uFEB0', '\uFEB1', '\uFEB2', '\uFEB3', '\uFEB4', '\uFEB5',
-        '\uFEB6', '\uFEB7', '\uFEB8', '\uFEB9', '\uFEBA', '\uFEBB', '\uFEBC', '\uFEBD', '\uFEBE',
-        '\uFEBF', '\uFEC0', '\uFEC1', '\uFEC2', '\uFEC3', '\uFEC4', '\uFEC5', '\uFEC6', '\uFEC7',
-        '\uFEC8', '\uFEC9', '\uFECA', '\uFECB', '\uFECC', '\uFECD', '\uFECE', '\uFECF', '\uFED0',
-        '\uFED1', '\uFED2', '\uFED3', '\uFED4', '\uFED5', '\uFED6', '\uFED7', '\uFED8', '\uFED9',
-        '\uFEDA', '\uFEDB', '\uFEDC', '\uFEDD', '\uFEDE', '\uFEDF', '\uFEE0', '\uFEE1', '\uFEE2',
-        '\uFEE3', '\uFEE4', '\uFEE5', '\uFEE6', '\uFEE7', '\uFEE8', '\uFEE9', '\uFEEA', '\uFEEB',
-        '\uFEEC', '\uFEED', '\uFEEE', '\uFEEF', '\uFEF0', '\uFEF1', '\uFEF2', '\uFEF3', '\uFEF4',
-        '\uFEF5', '\uFEF6', '\uFEF7', '\uFEF8', '\uFEF9', '\uFEFA', '\uFEFB', '\uFEFC'
+    // Presentation Forms-B Unicode ranges
+    private static final char[][] ARABIC_FORMS = {
+        // {isolated, final, initial, medial}
+        {'\u0627', '\uFE8E', '\uFE8D', '\uFE8D', '\uFE8D'}, // Alef
+        {'\u0628', '\uFE90', '\uFE91', '\uFE92', '\uFE91'}, // Beh
+        {'\u062A', '\uFE96', '\uFE97', '\uFE98', '\uFE97'}, // Teh
+        {'\u062B', '\uFE9A', '\uFE9B', '\uFE9C', '\uFE9B'}, // Theh
+        {'\u062C', '\uFE9E', '\uFE9F', '\uFEA0', '\uFE9F'}, // Jeem
+        {'\u062D', '\uFEA2', '\uFEA3', '\uFEA4', '\uFEA3'}, // Hah
+        {'\u062E', '\uFEA6', '\uFEA7', '\uFEA8', '\uFEA7'}, // Khah
+        {'\u062F', '\uFEAA', '\uFEA9', '\uFEA9', '\uFEA9'}, // Dal
+        {'\u0630', '\uFEAC', '\uFEAB', '\uFEAB', '\uFEAB'}, // Thal
+        {'\u0631', '\uFEAE', '\uFEAD', '\uFEAD', '\uFEAD'}, // Reh
+        {'\u0632', '\uFEB0', '\uFEAF', '\uFEAF', '\uFEAF'}, // Zain
+        {'\u0633', '\uFEB2', '\uFEB3', '\uFEB4', '\uFEB3'}, // Seen
+        {'\u0634', '\uFEB6', '\uFEB7', '\uFEB8', '\uFEB7'}, // Sheen
+        {'\u0635', '\uFEBA', '\uFEBB', '\uFEBC', '\uFEBB'}, // Sad
+        {'\u0636', '\uFEBE', '\uFEBF', '\uFEC0', '\uFEBF'}, // Dad
+        {'\u0637', '\uFEC2', '\uFEC3', '\uFEC4', '\uFEC3'}, // Tah
+        {'\u0638', '\uFEC6', '\uFEC7', '\uFEC8', '\uFEC7'}, // Zah
+        {'\u0639', '\uFECA', '\uFECB', '\uFECC', '\uFECB'}, // Ain
+        {'\u063A', '\uFECE', '\uFECF', '\uFED0', '\uFECF'}, // Ghain
+        {'\u0641', '\uFED2', '\uFED3', '\uFED4', '\uFED3'}, // Feh
+        {'\u0642', '\uFED6', '\uFED7', '\uFED8', '\uFED7'}, // Qaf
+        {'\u0643', '\uFEDA', '\uFEDB', '\uFEDC', '\uFEDB'}, // Kaf
+        {'\u0644', '\uFEDE', '\uFEDF', '\uFEE0', '\uFEDF'}, // Lam
+        {'\u0645', '\uFEE2', '\uFEE3', '\uFEE4', '\uFEE3'}, // Meem
+        {'\u0646', '\uFEE6', '\uFEE7', '\uFEE8', '\uFEE7'}, // Noon
+        {'\u0647', '\uFEEA', '\uFEEB', '\uFEEC', '\uFEEB'}, // Heh
+        {'\u0648', '\uFEEE', '\uFEED', '\uFEED', '\uFEED'}, // Waw
+        {'\u064A', '\uFEF2', '\uFEF3', '\uFEF4', '\uFEF3'}, // Yeh
+        {'\u0649', '\uFEEF', '\uFBE8', '\uFBE8', '\uFBE8'}, // Alef Maqsura
+        {'\u0623', '\uFE84', '\uFE83', '\uFE83', '\uFE83'}, // Alef with Hamza Above
+        {'\u0625', '\uFE88', '\uFE87', '\uFE87', '\uFE87'}, // Alef with Hamza Below
+        {'\u0622', '\uFE82', '\uFE81', '\uFE81', '\uFE81'}, // Alef Madda
+        {'\u0629', '\uFE94', '\uFE93', '\uFE93', '\uFE93'}, // Teh Marbuta
+        {'\u0640', '\u0640', '\u0640', '\u0640', '\u0640'}  // Tatweel
     };
 
-    // Arabic letters and their forms
-    private static final Map<Character, GlyphForms> ARABIC_FORMS_MAP = new HashMap<>();
-    static {
-        // فقط جزء من الحروف هنا كمثال، يجب نقل كل الحروف من كود المكتبة الأصلية
-        ARABIC_FORMS_MAP.put('\u0627', new GlyphForms('\uFE8D', '\uFE8E', '\uFE8D', '\uFE8D')); // Alef
-        ARABIC_FORMS_MAP.put('\u0628', new GlyphForms('\uFE8F', '\uFE90', '\uFE91', '\uFE92')); // Beh
-        ARABIC_FORMS_MAP.put('\u062A', new GlyphForms('\uFE95', '\uFE96', '\uFE97', '\uFE98')); // Teh
-        ARABIC_FORMS_MAP.put('\u062B', new GlyphForms('\uFE99', '\uFE9A', '\uFE9B', '\uFE9C')); // Theh
-        ARABIC_FORMS_MAP.put('\u062C', new GlyphForms('\uFE9D', '\uFE9E', '\uFE9F', '\uFEA0')); // Jeem
-        ARABIC_FORMS_MAP.put('\u062D', new GlyphForms('\uFEA1', '\uFEA2', '\uFEA3', '\uFEA4')); // Hah
-        ARABIC_FORMS_MAP.put('\u062E', new GlyphForms('\uFEA5', '\uFEA6', '\uFEA7', '\uFEA8')); // Khah
-        ARABIC_FORMS_MAP.put('\u0644', new GlyphForms('\uFEDD', '\uFEDE', '\uFEDF', '\uFEE0')); // Lam
-        ARABIC_FORMS_MAP.put('\u0645', new GlyphForms('\uFEE1', '\uFEE2', '\uFEE3', '\uFEE4')); // Meem
-        ARABIC_FORMS_MAP.put('\u0646', new GlyphForms('\uFEE5', '\uFEE6', '\uFEE7', '\uFEE8')); // Noon
-        ARABIC_FORMS_MAP.put('\u0647', new GlyphForms('\uFEE9', '\uFEEA', '\uFEEB', '\uFEEC')); // Heh
-        ARABIC_FORMS_MAP.put('\u0648', new GlyphForms('\uFEED', '\uFEEE', '\uFEED', '\uFEED')); // Waw
-        ARABIC_FORMS_MAP.put('\u064A', new GlyphForms('\uFEF1', '\uFEF2', '\uFEF3', '\uFEF4')); // Yeh
-        // ... أضف باقي الحروف من المكتبة الأصلية هنا ...
-    }
-
-    private static class GlyphForms {
-        char isolated, finalForm, initial, medial;
-        GlyphForms(char isolated, char finalForm, char initial, char medial) {
-            this.isolated = isolated;
-            this.finalForm = finalForm;
-            this.initial = initial;
-            this.medial = medial;
-        }
-    }
-
     private static boolean isArabicChar(char c) {
-        return ARABIC_FORMS_MAP.containsKey(c);
+        for (char[] form : ARABIC_FORMS) {
+            if (form[0] == c) return true;
+        }
+        return false;
     }
 
     private static char getForm(char c, int form) {
-        GlyphForms forms = ARABIC_FORMS_MAP.get(c);
-        if (forms == null) return c;
-        switch (form) {
-            case 0: return forms.isolated;
-            case 1: return forms.finalForm;
-            case 2: return forms.initial;
-            case 3: return forms.medial;
-            default: return c;
+        for (char[] forms : ARABIC_FORMS) {
+            if (forms[0] == c) return forms[form];
         }
+        return c;
     }
 
-    // --- نهاية كود Better-Arabic-Reshaper (Java port) ---
-
-    /**
-     * تعيد النص العربي بشكل صحيح (تشكيل احترافي)
-     */
     public static String reshapeArabic(String text) {
         if (text == null) return "";
         char[] chars = text.toCharArray();
@@ -91,7 +73,7 @@ public class BetterArabicReshaper {
             boolean prevArabic = (i > 0) && isArabicChar(chars[i - 1]);
             boolean nextArabic = (i < chars.length - 1) && isArabicChar(chars[i + 1]);
             if (!prevArabic && !nextArabic) {
-                sb.append(getForm(curr, 0)); // isolated
+                sb.append(getForm(curr, 1)); // isolated
             } else if (!prevArabic && nextArabic) {
                 sb.append(getForm(curr, 2)); // initial
             } else if (prevArabic && !nextArabic) {
