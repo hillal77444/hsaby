2bb0504959fd66a5917ef96c0a97f95d8f106a98
hillal77444 <63117152+hillal77444@users.noreply.github.com>
2025-07-20 21:44:26 +0300

    ل

--- android_app/app/src/main/java/com/hsaby/accounting/data/remote/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.hsaby.accounting.data.remote
-
-sealed class Result<out T> {
-    data class Success<out T>(val data: T) : Result<T>()
-    data class Error(val message: String) : Result<Nothing>()
-    object Loading : Result<Nothing>()
-} 
